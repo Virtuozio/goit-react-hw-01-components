fdba75d912109c1181668bdc66da578649a34cee
Bogdan Nazarchuk <111483751+Virtuozio@users.noreply.github.com>
2023-03-19 09:23:08 +0200

    styles added

--- src/components/friendlist/FriendListItem.jsx
+++ src/components/friendlist/FriendListItem.jsx
@@ -2,7 +2,11 @@ import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => (
   <li className={css.item}>
-    <span className={css.status}>{isOnline}</span>
+    {isOnline ? (
+      <span className={css.online}></span>
+    ) : (
+      <span className={css.offline}></span>
+    )}
     <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
     <p className={css.name}>{name}</p>
   </li>
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -5,14 +5,20 @@ export const Statistics = props => (
     {props.title && <h2 className={css.title}>{props.title}</h2>}
     <ul className={css['stat-list']}>
       {props.stats.map(obj => (
-        <li key={obj.id} className="item">
+        <li key={obj.id} className={css.item}>
           <span className={css.label}>{obj.label}</span>
-          <span className={css.percentage}>{obj.percentage}</span>
+          <span className={css.percentage}>{obj.percentage}%</span>
         </li>
       ))}
     </ul>
   </section>
 );
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 Statistics.propTypes = {
   stats: PropTypes.array,
