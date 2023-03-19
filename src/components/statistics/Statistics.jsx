f72da1c73a0be950c0d839646876366446915a6d
Bogdan Nazarchuk <111483751+Virtuozio@users.noreply.github.com>
2023-03-19 09:29:38 +0200

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -14,11 +14,11 @@ export const Statistics = props => (
   </section>
 );
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
 Statistics.propTypes = {
   stats: PropTypes.array,
